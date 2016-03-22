e518b41ded25dcc9d159bf5625aeb4ed673a3129
Juntao Wu <wujuntaocn@live.cn>
2016-03-23 02:11:45 +0800

    changes for SPA part of code

--- AngularSPA/Step6/cardsControllers.js
+++ AngularSPA/Step6/cardsControllers.js
@@ -1,21 +1,27 @@
-﻿
-var cardsControllers = angular.module("cardsControllers", []);
-
-cardsControllers
-    .controller("cardsListController", ["$scope", "Cards", function ($scope, Cards) {
-        $scope.cards = [];
-
-        Cards.query(function (d) {
-            var cards = [];
-            angular.forEach(d, function (block) {
-                angular.forEach(block.expansions, function (expansion) {
-                    angular.forEach(expansion.cards, function (card) {
-                        cards.push(card);
-                    });
+﻿(function () {
+    function processResult(d) {
+        var cards = [];
+        angular.forEach(d, function (block) {
+            angular.forEach(block.expansions, function (expansion) {
+                angular.forEach(expansion.cards, function (card) {
+                    cards.push(card);
                 });
             });
+        });
+        return cards;
+    }
 
-            $scope.cards = cards;
+    var cardsControllers = angular.module("cardsControllers", []);
+
+    cardsControllers.controller("cardsListController", ["$scope", "Cards", function ($scope, Cards) {
+        $scope.cards = [];
+        new Promise(function (resolve, reject) {
+            Cards.query(function (d) {
+                resolve(processResult(d));
+            });
+        }).then(function (data) {
+            $scope.cards = data;
+            $scope.$digest();
         });
 
         $scope.cardsCount = 10;
@@ -24,14 +30,31 @@ cardsControllers
         $scope.applyFilter = function () {
             console.log($scope.filterForm.cardsCount.$error);
         };
-    }])
-    .controller("cardDetailController", function ($scope, $routeParams) {
-        $scope.card = {};
-        var cards = $scope.$parent.cards;
-        var card = cards.filter(function (i) {
-            return i.id === $routeParams.cardId;
-        });
-        card = card.length ? card[0] : {};
-        
-        $scope.card = card;
-    });
+    }]);
+
+    cardsControllers.controller("cardDetailController", ["$scope", "Cards", "$routeParams",
+        function ($scope, Cards, $routeParams) {
+            $scope.card = {};
+            var cards = $scope.$parent.cards;
+
+            new Promise(function (resolve, reject) {
+                if (!cards) {
+                    Cards.query(function (d) {
+                        resolve(processResult(d));
+                    });
+                }
+                else {
+                    resolve(cards);
+                }
+            }).then(function (data) {
+                var card = data.filter(function (i) {
+                    return i.id == $routeParams.cardId;
+                });
+                card = card.length ? card[0] : {};
+
+                $scope.card = card;
+                $scope.$digest();
+            });
+        }]);  //end of cardDetailController
+
+})();
